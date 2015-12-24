de20e9ec58fdac738795e7b593e6863b54fb223f
Angus Tasker <atasker2@gmail.com>
2015-12-23 22:57:37 -0500

    Added REST for Competitions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @categories = Category.all
+    @competitions = Competition.all
   end
 
 end
--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,7 +1,11 @@
 class Competition < ActiveRecord::Base
 
   has_many :teamsartists
+  has_one :category
 
-  validates :name, presence: true
+  validates :name, presence: true,
+                   length: { maximum: 20 }
+
+  validates :category_id, presence: true
 
 end
