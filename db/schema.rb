897ee68c032a1547efba36def13fc09b99447f06
Angus Tasker <atasker2@gmail.com>
2016-01-04 15:05:32 -0500

    Change ticket category to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230034358) do
+ActiveRecord::Schema.define(version: 20160104200204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 20151230034358) do
 
   create_table "tickets", force: :cascade do |t|
     t.float    "price"
-    t.integer  "category"
+    t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "quantity"
