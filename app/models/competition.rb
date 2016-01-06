56a8ebe0e8e74a9f75f5980af5621608a945c5c4
Angus Tasker <atasker2@gmail.com>
2016-01-06 15:19:16 -0500

    Finalizing validations

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,6 +2,7 @@ class Competition < ActiveRecord::Base
 
   has_many :players
   has_one :category
+  has_many :events, dependent: :destroy
 
   validates :name, presence: true,
                    length: { maximum: 20 }
