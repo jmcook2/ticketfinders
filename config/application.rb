0add6b08636c81b9d010ac9ad399c46531d15389
Angus Tasker <atasker2@gmail.com>
2016-01-10 12:55:26 -0500

    Added custom font & associated CSS classes, also finished styling search box in top bar

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module Ticketfinders1
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Added to allow custom font
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
