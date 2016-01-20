694af9848acd52ffd721e889ac334d88df83a7bb
Angus Tasker <atasker2@gmail.com>
2016-01-20 15:07:00 -0500

    Fixed homepage grid & added top-bar logo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    @sports = Event.where(sports: true).take(6)
+    @sports = Event.where(sports: true).take(3)
+    @music_id = Category.where(description: "Music").ids
+    @music = Event.where(category_id: @music_id).take(3)
   end
 
 end
