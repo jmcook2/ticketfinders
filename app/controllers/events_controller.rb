02e654228c33b9eaf1537dbce6237f7a60480021
Angus Tasker <atasker2@gmail.com>
2016-01-21 15:03:43 -0500

    Additional breadcrumbs

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,8 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @category = Category.find(@event.category_id)
+    @category_description = Category.find(@event.category_id).description
   end
 
 end
