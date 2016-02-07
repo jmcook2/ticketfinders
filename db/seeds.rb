c2cf6c4d7e17e9708b172edd591bf6da5cef3b87
Angus Tasker <atasker2@gmail.com>
2016-02-07 10:34:16 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#################STAGE1##############################################
+
 categories = ['Music', 'Opera', 'Theater', 'Concerts']
 sports =     ['American Football', 'Baseball', 'Basketball', 'Boxing', 'Cricket', 'Football',
               'Golf', 'Hockey', 'Horse Racing', 'MMA', 'Motorcycling', 'Motor Racing', 'Olympics', 'Rugby',
@@ -8,26 +10,39 @@ end
 categories.each do |category|
   Category.create(description: category, sports: false)
 end
-football_competitions = ['UEFA Cup', 'Premier League', 'FA Cup']
-football_competitions.each do |comp|
-  Competition.create(name: comp, category_id: Category.where(description: "Football").ids)
-end
-opera_competitions = ['La Scala', 'Sydney Opera House']
-opera_competitions.each do |opera_comp|
-  Competition.create(name: opera_comp, category_id: Category.where(description: "Opera").ids)
-end
-football_players = ['Arsenal', 'Manchester Utd', 'West Ham', 'Newcastle', 'Everton']
-concerts_players = ['Madonna', 'U2', 'Kings of Leon', 'Enya', 'Kate Bush', 'Adele']
-football_players.each do |player|
-  Player.create(name: player, category_id: Category.where(description: "Football").ids)
-end
-concerts_players.each do |singer|
-  Player.create(name: singer, category_id: Category.where(description: "Concerts").ids)
-end
 venues = ['Hammersmith Apollo', 'Fenway Park', 'TD Garden', 'London Palladium', 'Royal Albert Hall',
           'O2 Arena', 'Old Trafford', 'Emirates', 'Sydney Opera House', 'Paris Opera House']
 venues.each do |venue|
   Venue.create(name: venue, capacity: rand(40000), city: 'Boston', country: 'USA', address: 'Boston, USA')
 end
+
+#####################################################################
+################STAGE2###############################################
+
+football_competitions = ['UEFA Cup', 'Premier League', 'FA Cup']
+football_competitions.each do |comp|
+  Competition.create(name: comp, category_id: Category.where(description: "Football").ids.pop)
+end
+opera_competitions = ['La Scala', 'Sydney Opera House']
+opera_competitions.each do |opera_comp|
+  Competition.create(name: opera_comp, category_id: Category.where(description: "Opera").ids.pop)
+end
+
+#####################################################################
+###############STAGE3################################################
+
+football_players = ['Arsenal', 'Manchester Utd', 'West Ham', 'Newcastle', 'Everton']
+concerts_players = ['Madonna', 'U2', 'Kings of Leon', 'Enya', 'Kate Bush', 'Adele']
+football_players.each do |player|
+  Player.create(name: player, category_id: Category.where(description: "Football").ids.pop)
+end
+concerts_players.each do |singer|
+  Player.create(name: singer, category_id: Category.where(description: "Concerts").ids.pop)
+end
+
+#####################################################################
+##############STAGE4#################################################
+
+
 # Use to create random datetimes for events
 # rand(6.months).seconds.from_now
