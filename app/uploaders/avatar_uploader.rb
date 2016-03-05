24561dbf985a304da2f8cde54d4508fb5aa47164
Angus Tasker <atasker2@gmail.com>
2016-03-05 17:10:03 -0500

    grid updates

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -14,10 +14,16 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fill: [400, 200]
-
   version :tiny do
     process resize_to_fill: [160, 100]
+  end
+
+  version :grid_large do
+    process resize_to_fill: [400, 450]
+  end
+
+  version :grid_small do
+    process resize_to_fill: [400, 300]
   end
 
 end
