90d7900c61ab32a39f598ffab05c141271402a43
Angus Tasker <atasker2@gmail.com>
2016-03-05 16:23:09 -0500

    table and gmail smtp update

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,7 +2,7 @@ class MessageMailer < ApplicationMailer
 
   def send_email(message)
     @message = message
-    recipients = ["john@ticket-finders.com"]
+    recipients = ["john@ticket-finders.com", "info@ticket-finders.com"]
     mail(:to => recipients, :subject => "New contact form submission at ticket-finders.com")
   end
 
