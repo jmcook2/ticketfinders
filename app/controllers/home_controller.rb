ed96a8291b83c0de4d17317f9b7bad400c4a7de7
Angus Tasker <atasker2@gmail.com>
2016-03-06 14:22:39 -0500

    insig

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    @sports = Event.where(sports: true).sample(3)
-    @concerts_id = Category.where(description: "Concerts").ids
-    @music = Event.where(category_id: @concerts_id).sample(3)
+    # @sports = Event.where(sports: true).sample(3)
+    # @concerts_id = Category.where(description: "Concerts").ids
+    # @music = Event.where(category_id: @concerts_id).sample(3)
   end
 
 end
