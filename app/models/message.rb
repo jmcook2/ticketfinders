43821aa52912fa624d56860ad53966f0d9a299b6
Angus Tasker <atasker2@gmail.com>
2016-03-19 15:11:13 -0400

    continued styling and updates

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 40 }
   validates :email, presence: true, length: { maximum: 40 }
+  validates :email, :format => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates :body, presence: true, length: { maximum: 500 }
 
 end
